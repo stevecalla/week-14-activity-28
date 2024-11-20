91ed604e93ec51e80940c9030951ba8aff40d981
Steve Calla <callasteven@gmail.com>
2024-11-20 07:48:36 -0700

    Adjust navbar tsx to use link

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,25 +1,47 @@
+import { Link } from 'react-router-dom';
 
 const NavBar = (props:any) => {
 
     const { loginToken, setLoginToken } = props.context;
 
+    const handleLogOut = () => {
+      localStorage.removeItem('LOGIN_TOKEN');
+      setLoginToken(null);
+    };
+
     return (
-        <nav id="navBar" className="navBar">
-            <h2 id="navTitle" className="navTitle">Shonen Showdown</h2>
-            <ul id="navList" className="navList">
-                <li id="navDash" className="navDash"><a href="/">Battle Dash</a></li>
-                {loginToken && <li id="navTeams" className="navTeams"><a href="/teamsRoom">Locker Room</a></li>}
-                {loginToken && <li id="navBattle" className="navBattle"><a href="/battleRoom">Battle Room</a></li>}
-                {loginToken ? 
-                (<li id="navLogOut" className="navLogOut"><a href="/login" onClick={() => {
-                    localStorage.removeItem('LOGIN_TOKEN');
-                    setLoginToken(null);
-                }}>Log Out</a></li>) : null}
-                {!loginToken && <li id="navLogin" className="navLogin"><a href="/login">Login</a></li>}
-                {!loginToken && <li id="navSignUp" className="navSignUp"><a href="/signUp">SignUp</a></li>}
-            </ul>
-        </nav>
+      <nav id="navBar" className="navBar">
+        <h2 id="navTitle" className="navTitle">Shonen Showdown</h2>
+        <ul id="navList" className="navList">
+          <li className="navDash">
+            <Link to="/" aria-label="Go to Battle Dash">Battle Dash</Link>
+          </li>
+          {loginToken && (
+            <>
+              <li className="navTeams">
+                <Link to="/teamsRoom" aria-label="Go to Locker Room">Locker Room</Link>
+              </li>
+              <li className="navBattle">
+                <Link to="/battleRoom" aria-label="Go to Battle Room">Battle Room</Link>
+              </li>
+              <li className="navLogOut">
+                <Link to="/login" onClick={handleLogOut} aria-label="Log out of the application">Log Out</Link>
+              </li>
+            </>
+          )}
+          {!loginToken && (
+            <>
+              <li className="navLogin">
+                <Link to="/login" aria-label="Go to Login Page">Login</Link>
+              </li>
+              <li className="navSignUp">
+                <Link to="/signUp" aria-label="Go to Sign Up Page">SignUp</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     );
-};
+  };
 
 export default NavBar;
